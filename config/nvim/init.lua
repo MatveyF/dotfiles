6ea26d639d56c320da0cd6d93c69a57755449f77
Matvey Fedoseev <matthew.fedoseev@gmail.com>
2025-04-09 19:03:38 +0100

    feat: [neovim] pylsp instead of pyright, none-ls instead of null-ls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -831,19 +831,21 @@ require("lazy").setup({
 				-- ts_ls = {},
 
 				-- ─────────────────────────────────── PYTHON ───────────────────────────────────
-				pyright = {
+				pylsp = {
 					settings = {
-						python = {
-							analysis = {
-								autoSearchPaths = true,
-								useLibraryCodeForTypes = true,
-								typeCheckingMode = "basic",
+						pylsp = {
+							plugins = {
+								pyflakes = { enabled = false },
+								pycodestyle = { enabled = false },
+								autopep8 = { enabled = false },
+								yapf = { enabled = false },
+								mccabe = { enabled = false },
+								pylsp_mypy = { enabled = false },
+								pylsp_black = { enabled = false },
+								pylsp_isort = { enabled = false },
 							},
 						},
 					},
-				},
-				ruff = {
-					line_length = 120,
 				},
 
 				-- ───────────────────────────────────── LUA ─────────────────────────────────────
@@ -878,6 +880,10 @@ require("lazy").setup({
 					},
 				},
 				eslint = {}, -- Automatically lint TypeScript files
+
+				-- ───────────────────────────────────── OTHER ─────────────────────────────────────
+				sqlls = {},
+				yamlls = {},
 			}
 
 			-- Ensure the servers and tools above are installed
@@ -1115,26 +1121,58 @@ require("lazy").setup({
 		},
 	},
 	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = function()
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+			"jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
+		},
+		config = function()
 			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.isort,
-					null_ls.builtins.formatting.black.with({
-						extra_args = { "--line-length=120" },
-					}),
-					null_ls.builtins.diagnostics.ruff.with({
-						extra_args = { "--line-length=120" },
-					}),
-					null_ls.builtins.formatting.prettier,
+			local formatting = null_ls.builtins.formatting -- to setup formatters
+			local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+
+			-- list of formatters & linters for mason to install
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"checkmake",
+					"prettier", -- ts/js formatter
+					"stylua", -- lua formatter
+					"eslint_d", -- ts/js linter
+					"shfmt",
+					"ruff",
 				},
+				-- auto-install configured formatters & linters (with null-ls)
+				automatic_installation = true,
 			})
 
-			vim.keymap.set("n", "<leader>lf", function()
-				vim.lsp.buf.format({ async = true })
-			end, { desc = "[L]int/[F]ormat current buffer" })
+			local sources = {
+				diagnostics.checkmake,
+				formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
+				formatting.stylua,
+				formatting.shfmt.with({ args = { "-i", "4" } }),
+				formatting.terraform_fmt,
+				require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
+				require("none-ls.formatting.ruff_format"),
+			}
+
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			null_ls.setup({
+				-- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+				sources = sources,
+				-- you can reuse a shared lspconfig on_attach callback here
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
+			})
 		end,
 	},
 
